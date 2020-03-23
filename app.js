62a246d254c8e593847fa26b336db836a15db7e7
Odomfavour <ogochukwuodom@gmail.com>
2020-03-23 01:13:33 +0100

    first javascript task showing name and courses

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+const fullName = 'Ogochukwu Odom';
+
+const myCourses = ['HTML', 'CSS', 'Javascript', 'Node Js'];
+
+console.log('My name is ' + fullName);
+console.log('The courses I do are: ');
+myCourses.forEach((i) => {
+  console.log(i);
+})
+
+for(let i = 1; i <= 200; i++) {
+  if((i % 2) === 0) {
+    console.log(i)
+  }
+  
+}
+
