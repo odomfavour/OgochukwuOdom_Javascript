e75b5f7a498ae91dfcf7f2e446ada634bd61ef3d
Odomfavour <ogochukwuodom@gmail.com>
2020-03-23 10:55:04 +0100

    list of courses and even numbers

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const fullName = 'Ogochukwu Odom';
 const myCourses = ['HTML', 'CSS', 'Javascript', 'Node Js'];
 
 console.log('My name is ' + fullName);
+console.log('\n')
 console.log('The courses I do are: ');
 myCourses.forEach((i) => {
   console.log(i);
 })
+
+console.log('\n')
 
 console.log('Below are even numbers between 1 and 200')
 for(let i = 1; i <= 200; i++) {
