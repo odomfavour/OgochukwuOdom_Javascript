027d88c28aa8796973a9b7da169a89b058f0c286
Odomfavour <ogochukwuodom@gmail.com>
2020-04-06 00:14:44 +0100

    simple interest function created

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 
 
-// const fullName = 'Ogochukwu Odom';
+const fullName = 'Ogochukwu Odom';
 
-// const myCourses = ['HTML', 'CSS', 'Javascript', 'Node Js'];
+const myCourses = ['HTML', 'CSS', 'Javascript', 'Node Js'];
 
-// console.log('My name is ' + fullName);
-// console.log('\n')
-// console.log('The courses I do are: ');
-// myCourses.forEach((i) => {
-//   console.log(i);
-// })
+console.log('My name is ' + fullName);
+console.log('\n')
+console.log('The courses I do are: ');
+myCourses.forEach((i) => {
+  console.log(i);
+})
 
-// console.log('\n')
+console.log('\n')
 
-// console.log('Below are even numbers between 1 and 200')
-// for(let i = 1; i <= 200; i++) {
-//   if((i % 2) === 0) {
-//     console.log(i)
-//   }
+console.log('Below are even numbers between 1 and 200')
+for(let i = 1; i <= 200; i++) {
+  if((i % 2) === 0) {
+    console.log(i)
+  }
   
-// }
+}
 
 
  
